fe943619f96de15980aa050b852690afb9d08f14
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-12-05 17:25:21 -0500

    modified contact form

---
+++ contact.php
@@ -0,0 +1,78 @@
+<!DOCTYPE <html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">      
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" media="screen">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>    
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script> 
+    <script type="text/javascript" src="app.js"></script>
+	<title>Contact</title>
+</head>
+<body class="contact">
+	<nav>
+    <div class="nav-wrapper #000000 black">
+      <a href="#" class="brand-logo center">Contact Me</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="index.html">Home</a></li>
+      </ul>
+    </div>
+    </nav>
+  <?php
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "SandersonAE@yahoo.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $message = $_REQUEST['message'];
+  
+  //send email
+  mail($admin_email, "$subject", $message, "From:" . $email);
+  
+  //Email response
+  echo "Thank you for contacting me!";
+  }
+  
+  //if "email" variable is not filled out, display the form
+  else  {
+?>
+<?php
+}
+?>
+  <div class="container">
+    <form name="contactform" method:"post" action:"contact.php" style= "margin-top:100px;">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="name" type="text" class="validate">
+          <label for="name">Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+       <div class="row">
+        <div class="input-field col s12">
+          <textarea id="message" class="materialize-textarea"></textarea>
+          <label for="message">Message</label>
+        </div>
+      </div> 
+      <button class="btn waves-effect waves-light center-align" type="submit" name="action">Submit
+          <i class="material-icons right">send</i>
+      </button> 
+      </form>
+    </div>
+ <!-- <div class="center">
+ 	<h2 class="email"><a href="mailto:SandersonAE@yahoo.com"> Email: SandersonAE@yahoo.com</a></h2>
+	<h2 class="email"><a href="mailto:anna.sanderson@csuglobal.edu"> Alt Email: anna.sanderson@csuglobal.edu</a></h2>
+</div>   -->
+<!-- <img src="http://i.imgur.com/DfxdPT3.jpg?1" /> -->
+
+
+ 
+</body>
+</html>
